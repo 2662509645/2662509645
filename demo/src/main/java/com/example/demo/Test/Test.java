6007431a2303b0ab388c85b893e70283033f6987
lve <2662509645@qq.com>
2023-05-06 10:27:48 +0800

    master新增test方法

---
+++ demo/src/main/java/com/example/demo/Test/Test.java
@@ -0,0 +1,12 @@
+package com.example.demo.Test;
+
+/**
+ * @author xiaoming xxx@163.com
+ * @version 2023/5/6 10:27
+ * @since JDK8
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
