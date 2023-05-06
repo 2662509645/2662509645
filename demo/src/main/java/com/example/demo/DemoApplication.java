3831bafbdf5f0d831b468fa5e52a7f88c0ee9797
lve <2662509645@qq.com>
2023-05-06 09:53:46 +0800

    list

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import  java.util.List;
+import  java.util.ArrayList;
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
+        List<String> list =new ArrayList();
         System.out.println("hello 111");
     }
 
